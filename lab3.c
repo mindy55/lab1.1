1ee68a8389bfc9515c5e72c177ac72dff1aeb4ac
nastya <39992260+mindy55@users.noreply.github.com>
2018-10-05 16:17:31 +0300

    Create lab3.c

---
+++ lab3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+
+
+float factorial(float n);
+int main(void)
+{
+double y=0,s=0,n=0,s1=0,sum=0;
+    for(double x= 0.1; x<=1; x+=0.1)
+    {
+        y = exp(cos(x))*cos(sin(x));
+        for(; n<=20; n++)
+      {
+            s +=cos(x)/factorial(n);
+      }
+      n=0;
+        do{
+            s1=cos(x)/factorial(n);
+            sum += s1;
+            n++;
+        }while(s1>0.0001);
+        
+     
+        printf("X= %.5lf  ",x);
+        printf("SN= %.5lf  ",s);
+        printf("SE= %.5lf  ",sum);
+        printf("Y= %.5lf\n",y);
+         n=1;
+         s=0;
+         s1=0;
+         sum=0;
+    }
+        
+}
+float factorial(float n)
+{
+    float f=1;
+    for(;n>0;n--){
+        f = f*n;
+    }       
+    return f;
+}                             
+            
